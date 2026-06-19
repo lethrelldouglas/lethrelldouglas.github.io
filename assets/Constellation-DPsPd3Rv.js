99dc1bcf1903f123c413eb971e372a48c17ef516
Lethrell <lethrell.douglas@triosstudent.com>
2026-06-19 16:45:39 -0400

    Reword Skills subtitle to avoid repeating the header

--- assets/Constellation-DPsPd3Rv.js
+++ assets/Constellation-DPsPd3Rv.js
@@ -1,4 +1,4 @@
-import{r as je,a as mx,g as gx,j as Yt}from"./index-DMZCrBGV.js";/**
+import{r as je,a as mx,g as gx,j as Yt}from"./index-Cd_e8-RI.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
